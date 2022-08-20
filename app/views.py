67471ae8f9213f2701d7d50049e51963566fc396
Tauqeer <2roshankhan6@gmail.com>
2022-08-20 03:17:44 +0100

    first

--- app/views.py
+++ app/views.py
@@ -1,19 +1,20 @@
 # Important imports
+from unittest import result
 from app import app
 from flask import request, render_template, url_for
 from keras import models
 import numpy as np
 from PIL import Image
-import Image
 import string
 import random
 import os
+import cv2
 
 # Adding path to config
 app.config['INITIAL_FILE_UPLOADS'] = 'app/static/uploads'
 
 # Loading model
-model = models.load_model('app/static/model/bird_species.h5')
+model = models.load_model('app/static/model/aur_1.h5')
 
 # Route to home page
 @app.route("/", methods=["GET", "POST"])
@@ -29,22 +30,39 @@ def index():
 
 		# Generating unique image name
 		letters = string.ascii_lowercase
-		name = ''.join(random.choice(letters) for i in range(10)) + '.png'
+		name = ''.join(random.choice(letters) for i in range(10)) + '.jpg'
 		full_filename =  'uploads/' + name
+		print(full_filename)
 
 		# Reading, resizing, saving and preprocessing image for predicition 
 		image_upload = request.files['image_upload']
+		print(type(image_upload))
 		imagename = image_upload.filename
 		image = Image.open(image_upload)
-		image = image.resize((224,224))
+		image11=np.array(image)
+		print('length',len(image11))
+		#image1 = cv2.imread('C:/Users/user/Pictures/Camera Roll/d.jpg')
+		#print('opencv-->',type(image1))
+		#image1 = cv2.resize(image1,(256,192))
+		#image = image.resize((192,256))
+		img = cv2.resize(image11,(256, 192))
+		print('image-->',type(img))
 		image.save(os.path.join(app.config['INITIAL_FILE_UPLOADS'], name))
-		image_arr = np.array(image.convert('RGB'))
-		image_arr.shape = (1,224,224,3)
+		#image_arr = np.array(image.convert('RGB'))
+		#image_arr.shape = (1,192,256,3)
 
 		# Predicting output
-		result = model.predict(image_arr)
+		#predi=model.predict(np.array([img]).astype(np.float32))
+        #print(predi)
+		result=model.predict(np.array([img]).astype(np.float32))
+		print(result)
+		#print('opencv--->',result1)
+		#result=model.predict(image_arr)
+		#result = model.predict(np.array([image]).astype(np.float32))
+		print('image-->',result)
 		ind = np.argmax(result)
-		classes = ['AMERICAN GOLDFINCH', 'BARN OWL', 'CARMINE BEE-EATER', 'DOWNY WOODPECKER', 'EMPEROR PENGUIN', 'FLAMINGO']
+		classes = ['Normal','Allergic you need use our Radiant Plump Soap']
+		print(ind)
 
 		# Returning template, filename, extracted text
 		return render_template('index.html', full_filename = full_filename, pred = classes[ind])
